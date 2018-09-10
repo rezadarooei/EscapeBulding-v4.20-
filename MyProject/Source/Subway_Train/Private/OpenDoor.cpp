cadb589d18750101d3275e568e7cde55ebdc799a
Daroi\REZA <reza.darooei@gmail.com>
2018-09-10 13:16:39 +0430

    065 Macros Starting with UPROPERTY

---
+++ MyProject/Source/Subway_Train/Private/OpenDoor.cpp
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AActor *Owner = GetOwner();
+	FVector Location = Owner->GetTransform().GetLocation();
+	Owner->SetActorLocation(FVector(Location.X,Location.Y+100,Location.Z));
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
